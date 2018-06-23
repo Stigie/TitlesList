2bf0db4f4cff0b7731f3ae83ff8543d7335251a1
Stigie <stiggie@info.sgu.ru>
2018-06-23 18:24:37 +0400

    add layout from png

---
+++ src/components/App/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Provider } from 'mobx-react';
+import styles from "./styles.module.css";
+import Form from '../Form/index'
+import Header from '../Header/index'
+import InputTextStore from '../../store/index';
+import Content from '../Content';
+import Footer from '../Footer';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Header/>
+        <Form/>
+        <Content/>
+        <Footer/>  
+      </div>
+    );
+  }
+}
+
+export default App;
