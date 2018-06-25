ae102cf95b62dc6465c52cd880142dfe607f8546
Stigie <stiggie@info.sgu.ru>
2018-06-25 11:47:48 +0400

    fix tags in Item

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -4,19 +4,14 @@ import styles from "./styles.module.css";
 class Item extends Component {
     render() {
         return (
-            < div className = { styles.item } >
+            <div className={styles.item} >
                 <div className={styles.title}>
                     Designation
-          </div>
+                </div>
                 <div className={styles.sourse}>
                     Find a co-founder for your sturt-up buisness
-          </div>
-          {/* <div className={styles.dark}>
-            <div className={styles.toptriangle}/>
-            <div className={styles.rectangular}/>
-            <div className={styles.bottomtriangle}/>
-          </div> */}
-        </div >
+                </div>
+            </div >
         )
     }
 }
