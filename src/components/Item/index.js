bc0ce175ecd48d856ed18570430eed3c0d3fc578
Petr Plodistyi <stiggie@info.sgu.ru>
2018-06-25 11:55:23 +0400

    Update index.js

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -4,14 +4,14 @@ import styles from "./styles.module.css";
 class Item extends Component {
     render() {
         return (
-            <div className={styles.item} >
+            <div className={styles.item}>
                 <div className={styles.title}>
                     Designation
                 </div>
                 <div className={styles.sourse}>
                     Find a co-founder for your sturt-up buisness
                 </div>
-            </div >
+            </div>
         )
     }
 }
