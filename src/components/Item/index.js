13ba7adf15d6d33eef64bf97c0e709a2c36c320f
Stigie <stiggie@info.sgu.ru>
2018-06-25 20:03:18 +0400

    fix mistakes

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -2,31 +2,17 @@ import React, { Component } from 'react';
 import styles from "./styles.module.css";
 
 class Item extends Component {
-<<<<<<< HEAD
   render() {
     return (
       <div className={styles.item} >
         <div className={styles.title}>
           Designation
-=======
-    render() {
-        return (
-            <div className={styles.item}>
-                <div className={styles.title}>
-                    Designation
->>>>>>> bc0ce175ecd48d856ed18570430eed3c0d3fc578
-                </div>
+        </div>
         <div className={styles.sourse}>
           Find a co-founder for your sturt-up buisness
-                </div>
-<<<<<<< HEAD
+        </div>
       </div >
     )
   }
-=======
-            </div>
-        )
-    }
->>>>>>> bc0ce175ecd48d856ed18570430eed3c0d3fc578
 }
 export default Item
