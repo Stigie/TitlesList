e74257f1036b00642188dd3764b9165951fc98b6
Stigie <stiggie@info.sgu.ru>
2018-06-25 21:17:54 +0400

    remove transfotm-react-statment

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,22 +4,32 @@ import styles from "./styles.module.css";
 import { observer, inject } from 'mobx-react';
 import InfoMassage from '../InfoMassage';
 
+
 @inject('TitelListStore')
 @observer
 class Content extends Component {
+  renderInfoBlock = () => {
+    if (this.props.TitelListStore.status !== "done") {
+      return (<InfoMassage status={this.props.TitelListStore.status} />);
+    }
+  }
+  renderListOfItems = () => {
+    let listToRender = this.props.TitelListStore.listOfTitles.map(
+      (item) => { 
+        return (<Item 
+          key={item.id} 
+          title={item.title} 
+          placeOfPublication={item.place_of_publication} />
+        )
+      }
+    );
+    return listToRender;
+  }
   render() {
-    const { TitelListStore } = this.props;
     return (
       <content className={styles.content}>
-        <If true={TitelListStore.state !== "done"}>
-          <InfoMassage state={TitelListStore.state} />
-        </If>
-        <For each="listOfTitles" in={TitelListStore.listOfTitles}>
-          <Item
-            key={listOfTitles.id}
-            title={listOfTitles.title}
-            placeOfPublication={listOfTitles.place_of_publication} />
-        </For>
+        { this.renderInfoBlock() }
+        { this.renderListOfItems() }
       </content>
     )
   }
--- src/components/InfoMassage/index.js
+++ src/components/InfoMassage/index.js
@@ -3,18 +3,27 @@ import styles from "./styles.module.css";
 import loadImg from '../../assets/load.gif'
 
 class InfoMassage extends Component {
+  renderLoad = () => {
+    if (this.props.status === "pending") {
+      return (<img src={loadImg} />);
+    }
+  }
+  renderError = () => {
+    if (this.props.status === "error") {
+      return ("Ошибка загрузки данных");
+    }
+  }
+  renderEmpty = () => {
+    if (this.props.status === "empty") {
+      return ("По запросу ничего не найдено");
+    }
+  }
   render() {
     return (
       <div className={styles.load}>
-        <If true={this.props.state === "pending"}>
-          <img src={loadImg} />
-        </If>
-        <If true={this.props.state === "error"}>
-          Ошибка загрузки данных
-        </If>
-        <If true={this.props.state === "empty"}>
-          По запросу ничего не найдено
-        </If>
+        { this.renderLoad() }
+        { this.renderError() }
+        { this.renderEmpty() }
       </div >
     )
   }
