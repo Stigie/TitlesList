fa977828a4732c69f30a903e5e0ba85d11d621ce
Stigie <stiggie@info.sgu.ru>
2018-07-01 23:13:27 +0400

    add json-server integration

--- src/store/index.js
+++ src/store/index.js
@@ -19,12 +19,13 @@ class TitleListStore {
   @action('click on submit, filter data')
   async clickOnsubmit() {
     this.status = "pending";
-    let url = `https://chroniclingamerica.loc.gov/search/titles/results/?terms=${this.inputText}&format=json&page=1`;
+    let url = `http://localhost:3000/titles?q=${this.inputText}`;
     this.listOfTitles = [];
     try {
       let response = await fetch(url);
       let jsonResponse = await response.json();
-      this.listOfTitles = jsonResponse.items.map((item) => { return new Title(item.id, item.title, item.place_of_publication) });
+      this.listOfTitles = jsonResponse.map((item) => { return new Title(item.id, item.title, item.placeOfPublication) });
+      console.log(this.listOfTitles);
       runInAction(() => {
         if (this.listOfTitles.length === 0) {
           this.status = "empty";
